afa9a886f4719c58d9c08d7a1fba0b0ee33266a3
mahfuz380 <almahfuz380@gmail.com>
2021-05-03 11:20:39 +0600

    mastering done

---
+++ resources/views/admin/include/script.blade.php
@@ -0,0 +1,21 @@
+<!-- Bootstrap core JavaScript-->
+<script src="frontend/vendor/jquery/jquery.min.js"></script>
+<script src="frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="frontend/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="frontend/js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="frontend/vendor/chart.js/Chart.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="frontend/js/demo/chart-area-demo.js"></script>
+<script src="frontend/js/demo/chart-pie-demo.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
